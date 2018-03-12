f3e915e6aea5758575aa6c587941ac0e129239e9
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-11 17:52:32 -0700

    added scripts for installing components

---
+++ metrics/influxdb/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker run -d --name=influxdb -p 8086:8086 -v Container_InfluxDB:/var/lib/influxdb influxdb
+sleep 5 # sleep for 5secs before attempting to create a database on influx
+curl -X POST 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=DROP DATABASE telegraf"
+curl -X POST 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=CREATE DATABASE telegraf"
+curl -X POST 'http://localhost:8086/query?pretty=true'  --data-urlencode "q=ALTER RETENTION POLICY autogen ON telegraf DURATION 24h SHARD DURATION 1h"
