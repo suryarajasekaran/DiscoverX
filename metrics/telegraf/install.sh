81dd559894b424daad7f255096df627cdc9e5160
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-13 01:09:04 -0700

    update

---
+++ metrics/telegraf/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
+[influxdb]
+name = InfluxDB Repository - RHEL \$releasever
+baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key
+EOF
+
+yum update
+yum install telegraf
