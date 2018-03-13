67fa43829f951b8d14996aa832438d68df5df81d
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-13 01:40:01 -0700

    added fixes

--- metrics/telegraf/install.sh
+++ metrics/telegraf/install.sh
@@ -5,3 +5,4 @@ kill -9 $(ps -ef | grep telegraf)
 wget https://dl.influxdata.com/telegraf/releases/telegraf-1.5.2-1.x86_64.rpm
 sudo yum localinstall telegraf-1.5.2-1.x86_64.rpm
 rm -rf telegraf-1.5.2-1.x86_64.rpm
+usermod -aG docker telegraf
