dd2128c65cc91e64ad637f47fa543c8bbfb7d92d
Sudhish  Kasaba Ramesh <sudhishkr@Sudhishs-MacBook-Pro.local>
2018-03-15 00:44:56 -0700

    fix

---
+++ load/alb/Load.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+from multiprocessing import Process
+import requests
+from random import randint
+
+webapp_end_point_dict = {
+                                "agent"    : ["/a/ping","/a/agent"],
+                                "user"    : ["/u/ping","/u/user"],
+                                "manager"  : ["/m/ping","/m/manager"],
+                                "frontend"   : ["/f/ping","/f/frontend"]
+                          }
+
+def induce_load_endpoint(end_point):
+    print "This is a induce load program"
+    requests.get("http://alb-680086830.us-west-1.elb.amazonaws.com:"+str(end_point[0]))
+
+
+def parallel_induce_load_endpoint(webapp_name, load_parallel):
+    p_list = []
+    for i in range(0, load_parallel):
+        p = Process(target=induce_load_endpoint, args=(webapp_end_point_dict[webapp_name],))
+        p.start()
+    for p in p_list:
+        p.join()
+
+
---
+++ load/alb/loadgenerator.py
@@ -0,0 +1,19 @@
+
+from Load import parallel_induce_load_endpoint
+from multiprocessing import Process
+import time
+
+if __name__ == '__main__':
+    try:
+        while (True):
+
+            p_list = []
+
+            p = Process(target=parallel_induce_load_endpoint, args=("agent", 30))
+            p.start()
+            p_list.append(p)
+
+            for p in p_list:
+                p.join()
+    except KeyboardInterrupt:
+        print('Stopping Load')
